9716e85ffe4201cbc7bef2e76893330db4e6d9e8
Dylan Shine <dylanshinenyc@gmail.com>
2015-07-27 17:13:05 -0400

    Create constants file for project

---
+++ Bubble/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  Bubble
+//
+//  Created by Dylan Shine on 7/27/15.
+//  Copyright (c) 2015 Bubble. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+@end
