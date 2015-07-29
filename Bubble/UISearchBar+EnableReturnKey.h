fd69700aec894624846e8e52ae803738f67d6056
lukasthoms <luk@stho.ms>
2015-07-29 17:14:14 -0400

    enabled return key always

---
+++ Bubble/UISearchBar+EnableReturnKey.h
@@ -0,0 +1,13 @@
+//
+//  UISearchBar+EnableReturnKey.h
+//  Bubble
+//
+//  Created by Lukas Thoms on 7/29/15.
+//  Copyright (c) 2015 Bubble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UISearchBar (EnableReturnKey)
+
+@end
---
+++ Bubble/UISearchBar+EnableReturnKey.m
@@ -0,0 +1,13 @@
+//
+//  UISearchBar+EnableReturnKey.m
+//  Bubble
+//
+//  Created by Lukas Thoms on 7/29/15.
+//  Copyright (c) 2015 Bubble. All rights reserved.
+//
+
+#import "UISearchBar+EnableReturnKey.h"
+
+@implementation UISearchBar (EnableReturnKey)
+
+@end
