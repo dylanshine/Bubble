540edd22d1d0e7ee2dd2a2407c0585e7f91b2e32
lukasthoms <luk@stho.ms>
2015-08-05 11:34:48 -0400

    fixed map scroll bug and added DatePicker animation

---
+++ Bubble/BBSearchViewPassThrough.m
@@ -0,0 +1,21 @@
+//
+//  BBSearchView.m
+//  Bubble
+//
+//  Created by Lukas Thoms on 8/5/15.
+//  Copyright (c) 2015 Bubble. All rights reserved.
+//
+
+#import "BBSearchView.h"
+
+@implementation BBSearchView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
