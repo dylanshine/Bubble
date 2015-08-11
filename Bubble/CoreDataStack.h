abc22515e34551781ddf419d705b1029e95128a8
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-11 10:03:44 -0400

    Add core data singleton to project

---
+++ Bubble/CoreDataStack.h
@@ -0,0 +1,23 @@
+//
+//  CoreDataStack.h
+//  Bubble
+//
+//  Created by Dylan Shine on 8/11/15.
+//  Copyright (c) 2015 Bubble. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface CoreDataStack : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
++(instancetype) sharedStack;
+
+@end
