e2fca62eb604af9fb6453be4c12eb20ab0ec5542
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-12 13:03:17 -0400

    Clean up ChatDataManager

--- Bubble/ChatDataManager.h
+++ Bubble/ChatDataManager.h
@@ -1,18 +1,10 @@
-//
-//  ChatDataManager.h
-//  Bubble
-//
-//  Created by Dylan Shine on 8/6/15.
-//  Copyright (c) 2015 Bubble. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 @interface ChatDataManager : NSObject
-@property (nonatomic) NSMutableArray *messages;
-@property (nonatomic) NSMutableDictionary *avatars;
+@property (strong, nonatomic) NSMutableArray *messages;
+@property (strong ,nonatomic) NSMutableDictionary *avatars;
 
-+(instancetype)sharedManager;
--(void)fetchUserProfilePictureWithFaceBookId:(NSString *)fbID Completion:(void (^)(UIImage *profileImage))block;
++ (instancetype)sharedManager;
+- (void)fetchUserProfilePictureWithFaceBookId:(NSString *)fbID Completion:(void (^)(UIImage *profileImage))block;
 @end
