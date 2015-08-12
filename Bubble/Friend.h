4b2a2664a432c6e27a2695d43e06fab2bc5fd930
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-12 12:52:13 -0400

    Clean up Friend model

--- Bubble/Friend.h
+++ Bubble/Friend.h
@@ -1,18 +1,10 @@
-//
-//  Friend.h
-//  Bubble
-//
-//  Created by Dylan Shine on 8/10/15.
-//  Copyright (c) 2015 Bubble. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 @interface Friend : NSObject
-@property (nonatomic) NSString *name;
-@property (nonatomic) NSString *facebookId;
-@property (nonatomic) UIImage *image;
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *facebookId;
+@property (strong, nonatomic) UIImage *image;
 
--(instancetype)initWithName:(NSString *)name FacebookId:(NSString *)facebookId;
+- (instancetype)initWithName:(NSString *)name FacebookId:(NSString *)facebookId;
 @end
