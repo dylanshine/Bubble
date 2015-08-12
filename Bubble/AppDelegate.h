5559bff12f088d7fd2d2bdc2774ec35e0ff6e2f6
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-12 12:17:20 -0400

    Clean up AppDelegate

--- Bubble/AppDelegate.h
+++ Bubble/AppDelegate.h
@@ -1,17 +1,6 @@
-//
-//  AppDelegate.h
-//  Bubble
-//
-//  Created by Lukas Thoms on 7/27/15.
-//  Copyright (c) 2015 Bubble. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-
 @property (strong, nonatomic) UIWindow *window;
-
-
 @end
 
