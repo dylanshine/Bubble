f72b70df1e6f9ed3d0ce0df896fb26dbdc3ae654
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-12 12:28:32 -0400

    Clean up LoginViewController

--- Bubble/LoginViewController.h
+++ Bubble/LoginViewController.h
@@ -1,13 +1,4 @@
-//
-//  LoginViewController.h
-//  Bubble
-//
-//  Created by Jordan Guggenheim on 7/31/15.
-//  Copyright (c) 2015 Bubble. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface LoginViewController : UIViewController
-
 @end
